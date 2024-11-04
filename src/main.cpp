d90801572818292226ca07b86bbd63a8df317e14
Jacob Zargooshifar <zargooshifar@outlook.com>
2024-11-04 19:58:43 +0100

    add time delay

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include <atomic>
 #include <thread>
 #include <iostream>
-
-
+#include <chrono>
 
 int main(int argc, char **argv)
 {
@@ -14,8 +13,12 @@ int main(int argc, char **argv)
    
     gpios.setGPIOCallback([]() -> void { std::cout << "a gpio changed!" << std::endl;});
     gpios.init();
-
-    gpios.set_output(GPIO_PIN::OUT2, 1);
+    
+    gpios.set_output(GPIO_PIN::OUT1, 0);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    gpios.set_output(GPIO_PIN::OUT1, 1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    gpios.set_output(GPIO_PIN::OUT1, 0);
 
 
     // wait for input to exit the program...
